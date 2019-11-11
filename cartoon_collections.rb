4508cc315b82dcb999af5b20a4f91d2f37df3f63
Anthony Pardo <pardo.anthony@gmail.com>
2019-11-11 20:05:32 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -29,8 +29,10 @@ def find_the_cheese(arr)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   i = 0 
-  
+  while i < 3
   if arr.include(cheese_types[i])
     return cheese_types[i]
-  
+  i += 1 
+  end
+  return nil 
 end
