3e08a0dbf4cdc57be92806344df713b42b4899b2
Anthony Pardo <pardo.anthony@gmail.com>
2019-11-11 19:22:15 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,7 @@ end
 
 def summon_captain_planet(calls)# code an argument here
   # Your code here
-  calls.collect()
+  calls.collect{ |call| call + "!" }
 end
 
 def long_planeteer_calls# code an argument here
